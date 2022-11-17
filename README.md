7b18bb3b63bbb58e8715338d127c15ebe306e9cf
Bas van Kervel <basvankervel@gmail.com>
2022-11-17 14:06:23 +0100

    update bindings generation instructions

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-# ThingsIX token go bindings
-This repository contains go bindings to interact with the `ThixM` and `Thix`
-token contracts.
+# Generate bindings
 
-## Generate bindings
-Run `go generate` from the root of the project. This will checkout the smart
-contract repository in a temporarly work directory and extracts the ABI from the
-token contracts and feeds it to abigen to generate the bindings. The bindings
-are stored in the `thixm` and `thix` packages.
+First clone the smart-contracts repository from `git@github.com:ThingsIXFoundation/smart-contracts-evm.git`
+and compile the contracts. This generated an `artifacts` directory that includes
+the data to generate the bindings.
 
-# Prerequisites
-The user needs to have access to the ThingsIX smart contract repository and
-needs to have `go` and `abigen` installed.
+The shell script must be passed the directory to the smart contracts project and
+will grab the relevant data from the `artifacts` directory and generated the
+bindings in `bindings.go`.
+
+## Example
+```
+./generate-bindings.sh $HOME/dev/thingsix/smart-contracts-evm
+```
--- generate-bindings.sh
+++ generate-bindings.sh
@@ -1,39 +1,46 @@
 #!/bin/sh
+# Copyright 2022 Stichting ThingsIX Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
 
 set -e
 
-THINGS_TOKEN_SMART_CONTRACT_REPO=git@github.com:ThingsIXFoundation/smart-contracts-evm.git
-
 generate_bindings() {
-  local work_dir=$1
-  local pkg=$2
-  local artifact_file=$(find $work_dir/artifacts -name "$3.json")
-  local abi_file="$work_dir/$3.abi"
+  local work_dir=$(mktemp -d /tmp/thingsix-router-registry-XXXXX)
+  local artifact_file=$(find $1/artifacts -name "$3.json")
+  local pkg=$2  
+  local abi_file="$work_dir/router_registry.abi"  
 
-  # ensure output directory exists
-  mkdir -p "$pkg"
-
-  # extract abi from artifacts file
+  echo Extract abi from artifact file
   jq .abi "$artifact_file" > "$abi_file"
 
-  echo "Generate bindings for package $pkg"
-  
-  abigen --abi "$abi_file" --pkg $pkg --out "$pkg/bindings.go"
+  echo Generate bindings for package $pkg
+  abigen --abi "$abi_file" --pkg $pkg --out "$2/bindings.go"
+
+  echo Delete work directory
+  rm -rf $work_dir
 }
 
-# clone smart contract repo and install dependencies
-work_dir=$(mktemp -d /tmp/thingsix-tokens-XXXXX)
-echo "Clone smart contract repo in work directory $work_dir"
-git clone $THINGS_TOKEN_SMART_CONTRACT_REPO $work_dir --quiet
+smart_contract_project=$1
+echo Use smart contract project in ${smart_contract_project}
 
-# generate go-bindings for ThixM token contract
-generate_bindings $work_dir thixm ThixM
-generate_bindings $work_dir thix Thix
-
-# cleanup work directory
-echo "delete work directory"
-rm -rf $work_dir
+# generate go-bindings for token contracts
+generate_bindings ${smart_contract_project} thixm ThixM
+generate_bindings ${smart_contract_project} thix Thix
 
 # install dependencies
-echo "tidy go dependencies"
+echo Cleanup go dependencies
 go mod tidy
